3991aaf96aa6b88414d7e75d2e2e73adbc1e4bda
Bart Slegt <109470782+Euforic-Coder@users.noreply.github.com>
2023-04-02 17:30:45 -0400

    Updated

---
+++ uforia/debug.h
@@ -0,0 +1,30 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <iostream>
+#include <chrono>
+
+// Print debug messages
+void debug(std::string s, bool b = true){
+	if(bool b = true){
+		std::cout << "\033[37m" << s << "\033[0m" << std::endl;
+	}else{
+		std::cout << "\033[31m" << s << "\033[0m" << std::endl;	
+	}
+}
+
+// Start the timer
+auto timer_start = std::chrono::high_resolution_clock::now();
+
+
+// Ends the timer
+auto timer_end = std::chrono::high_resolution_clock::now();
+
+
+// Prints the time duration
+void timer_print(){
+	std::chrono::duration<double, std::milli> elapsed = timer_end - timer_start; 
+	std::string s = "\033[37mElapsed Time: " + std::to_string(elapsed.count()) + " seconds\033[0m"; 
+	std::cout << s << std::endl;
+}
+#endif // DEBUG_H
--- uforia/strings.h
+++ uforia/strings.h
@@ -1,56 +1,76 @@
 #ifndef STRINGS_H
 #define STRINGS_H
 
+// Capitalize a string
 std::string capitalized(std::string s){
-    std::string buffer;
-    for(int i = 0; i < s.length(); ++i){
-        if(i == 0){
-            buffer.push_back(toupper(s[i]));
+  std::string buffer;
+  for(int i = 0; i < s.length(); ++i){
+      if(i == 0){
+          buffer.push_back(toupper(s[i]));
         }else{
-            buffer.push_back(tolower(s[i]));
+          buffer.push_back(tolower(s[i]));
         }
     }
-    return buffer;
+  return buffer;
 }
 
+// Makes a string lowercase
 std::string lowercase(std::string s){
-    std::string buffer;
-    for(int i = 0; i < s.length(); ++i){
-        buffer.push_back(tolower(s[i]));
+  std::string buffer;
+  for(int i = 0; i < s.length(); ++i){
+      buffer.push_back(tolower(s[i]));
     }
-    return buffer;
+  return buffer;
 }
 
+// Makes a string uppercase
 std::string uppercase(std::string s){
-    std::string buffer;
-    for(int i = 0; i < s.length(); ++i){
-        buffer.push_back(toupper(s[i]));
+  std::string buffer;
+  for(int i = 0; i < s.length(); ++i){
+      buffer.push_back(toupper(s[i]));
     }
-    return buffer;
+  return buffer;
 }
 
+// Prints a string with a given number of spaces
 std::string spacer(std::string s, int space){
-    std::string buffer;
-    buffer = s;
-    int total = space - s.length();
-    for(int i = 0; i < total; ++i){
-        buffer.push_back(' ');
+  std::string buffer;
+  buffer = s;
+  int total = space - s.length();
+  for(int i = 0; i < total; ++i){
+      buffer.push_back(' ');
     }
-    return buffer;
+  return buffer;
 }
 
+// Creates a vector<string> from a string with all possible combinations
+std::vector<std::string> funnel(const std::string &s) {
+  std::vector<std::string> buffer;
+  int full_length = s.length();
+  for (int i = 0; i < full_length; i++) {
+      size_t sub_length = full_length - i;
+      size_t play = full_length - sub_length;
+      for (int move_pos = 0; move_pos <= play; move_pos++) {
+          std::string sub_str = s.substr(move_pos, sub_length);
+          buffer.push_back(sub_str);
+        }
+    }
+  return buffer;
+}
+
+// Checks if a string has a number
 bool has_number(std::string s){
-    std::string numbers = "1234567890";
-    for(int i = 0; i < s.length(); ++i){
-        char character = s[i];
-        for(int j = 0; j < numbers.length(); ++j){
-            char number = numbers[j];
-            if(character == number){
-                return true;
+  std::string numbers = "1234567890";
+  for(int i = 0; i < s.length(); ++i){
+      char character = s[i];
+      for(int j = 0; j < numbers.length(); ++j){
+          char number = numbers[j];
+          if(character == number){
+              return true;
             }
         }
     }
-    return false;
+  return false;
 }
 
 #endif // STRINGS_H
